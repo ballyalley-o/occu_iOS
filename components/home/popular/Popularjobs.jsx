aff874ec423e75cf00fb4573e1a72a2595c9bc96
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-09-16 19:23:45 +1200

    ui: job card done, pre-api set up

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import { useState } from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native'
+import { useRouter } from 'expo-router'
+// components
+import PopularJobCard from '../../common/cards/popular/PopularJobCard'
+// styles
 import styles from './popularjobs.style'
+// constants
+import { COLORS, SIZES } from '../../../constants'
 
 const Popularjobs = () => {
+  const router = useRouter()
+  const isLoading = false
+  const error = false
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size='large' colors={COLORS.primary} />
+        ) : error ? (
+          <Text> Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
+        )}
+      </View>
     </View>
   )
 }
